6ae50b357be06888f18650f76b3497cdb8bd8526
jose-cipriano <webdev10250907gmail.com>
2022-12-25 07:04:15 -0300

    fix: escaped character

--- components/tabs/user-info.jsx
+++ components/tabs/user-info.jsx
@@ -4,7 +4,7 @@ export const UserInfo = ({ activeTab, user }) => {
       <span>User {activeTab} : </span>
       <span>{user[0][activeTab]}</span>
       <div>
-        <i>I don't know what information should be here</i>
+        <i>I don&apos;t know what information should be here</i>
       </div>
     </>
   );
